11b2658f71f38c8de96c80acf1252548fad73e7f
123arnav <74090758+123arnav@users.noreply.github.com>
2021-03-13 12:24:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
+
+tint_color="";
 function preload(){
 
 }
+function filter_img(){
+    tint_color=document.getElementById("choose_filter").value;
+}
 function setup(){
 canvas=createCanvas(1000,700);
-canvas.position(100,230);
+canvas.position(100,330);
 video=createCapture(VIDEO)
 video.hide()
 }
 function draw(){
+    tint(tint_color);
     image(video,200,100,600,500);
     fill(0,0,100)
 circle(0,0,360,)
